3e74776c38c2968e2666e4d539f9e748a808cded
saleem189 <saleemayoub1@gmail.com>
2024-04-22 20:17:42 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/Myportfolio/',
-  base: '/',
+  base: '/Myportfolio/',
   plugins: [
     vue(),
   ],
